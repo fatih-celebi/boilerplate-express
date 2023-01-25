a47ef2f550dc861f13adb4149987fc02ee24d3c9
fatih-celebi <celebifatih.c1@gmail.com>
2023-01-25 23:56:56 +0000

    Complete Challenges 1 to 3

--- myApp.js
+++ myApp.js
@@ -1,15 +1,23 @@
 let express = require('express');
 let app = express();
 
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+app.get("/", function (req, res) {
+  console.log("Hello World");
+});
+
+app.get("/", function (req, res) {
+  res.send("Hello Express");
+});
 
 
 
-
-
-
-
-
-
+const listener = app.listen(9000, function () {
+  console.log("Your app is listening on port" + listener.address().port);
+});
 
 
 
